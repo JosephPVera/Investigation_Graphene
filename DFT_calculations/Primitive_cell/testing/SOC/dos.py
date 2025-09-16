741cb112266088d5d46d748c2b4ee3fb30d9e4d5
Joseph P.Vera <joseph.pv114@gmail.com>
2025-09-17 01:47:51 +0200

    Add new folder and files

--- DFT_calculations/Primitive_cell/testing/SOC/dos.py
+++ DFT_calculations/Primitive_cell/testing/SOC/dos.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-EFermi =   -1.6793
+EFermi =   -1.6802
 
 # Load data, skipping the first line (header)
 data = np.loadtxt('graphene_dos.dat', comments='#')
@@ -17,14 +17,17 @@ energy = data[:, 0] - EFermi
 dos = data[:, 1]
 
 # Plot
-plt.figure(figsize=(6,4))
-plt.plot(energy, dos, color='blue')
-plt.xlabel('Energy (eV) - EFermi')
-plt.ylabel('DOS')
+plt.figure(figsize=(12,7)) # (6,4)
+plt.plot(energy, dos, color='xkcd:blue', linewidth=1)
+plt.axvline(x=0, color='xkcd:black', linestyle='--', linewidth=1) # Fermi energy
+plt.xlabel('Energy (eV)', fontsize=16)
+plt.ylabel('DOS (States/eV)', fontsize=16)
 #plt.title('Density of States for Graphene (rescaled energy)')
 #plt.grid(True)
-#plt.ylim(0, 10)
-#plt.xlim(-3, 3)
+#plt.ylim(0, 0.9)
+#plt.xlim(-2.5, 2)
+plt.xlim(-20, 15)
+plt.ylim(0, 1.85)
 plt.tight_layout()
 plt.savefig("graphene_dos.png", dpi=300)
 #plt.show()
