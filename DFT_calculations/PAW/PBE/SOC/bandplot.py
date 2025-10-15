55cee443a718ed973d66ab5e134b6d3105d763f0
JosephPVera <joseph.pv114@gmail.com>
2025-10-14 22:32:33 -0500

    Add new folder and files

--- DFT_calculations/PAW/PBE/SOC/bandplot.py
+++ DFT_calculations/PAW/PBE/SOC/bandplot.py
@@ -6,7 +6,7 @@ data = np.loadtxt('graphene_bands.dat.gnu')
 k = np.unique(data[:, 0])
 bands1 = np.reshape(data[:, 1], (-1, len(k)))
 
-EFermi = -1.6845
+EFermi = -1.7236
 bands = bands1 - EFermi
 
 #plt.figure(figsize=(12, 7))
@@ -20,17 +20,26 @@ plt.axhline(0, linestyle='--', linewidth=0.75, color='xkcd:black')
 
 # High symmetry k-points (check bands_pp.out)
 plt.axvline(0.6667, linewidth=0.2, color='xkcd:black')
-plt.axvline(1, linewidth=0.2, color='xkcd:black')
+plt.axvline(1.0000, linewidth=0.2, color='xkcd:black')
 
 # text labels
-plt.xticks(ticks= [0, 0.6667, 1, 1.5774], labels=[r'$\Gamma$', 'K', 'M', r'$\Gamma$'], fontsize=12)
-plt.ylabel("Energy (eV)", fontsize=14)
-#plt.xlim(0.666698, 0.666701)
-#plt.ylim(0.909595, 0.909605)
-plt.ylim(-20, 12)
+plt.xticks(ticks= [0, 0.6667, 1.000, 1.5774], labels=[r'$\Gamma$', 'K', 'M', r'$\Gamma$'], fontsize=12)
 
-#plt.xlim(0.65, 0.68)
-#plt.ylim(-0.1, 0.1)
+#plt.ylabel('Energy (eV)', fontsize=14)
+plt.ylabel(r'Energy ($\mu$eV)', fontsize=14)
+#plt.ylim(-20, 12)
+#plt.ylim(-10, 5)
+
+# check the cones -- 2 \mu eV
+#plt.xlim(0.66669992, 0.66670007)
+#plt.ylim(-0.000001, 0.000001)
+
+# check new cones -- 0.4 \mu eV
+plt.xlim(0.999999984, 1.000000014)
+plt.ylim(-0.0000002, 0.0000002)
+
+#plt.gca().get_yaxis().get_offset_text().set_visible(False)
+#plt.ticklabel_format(axis='y', style='sci', scilimits=(-6, -6)) # 10^{-6}
 
 plt.tight_layout()
 plt.savefig("graphene.png", dpi=200, bbox_inches='tight')
